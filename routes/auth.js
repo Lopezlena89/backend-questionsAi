47bdc249e2facf83921bf1c11ca1d96f0d5bf0d2
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2024-01-31 18:28:13 -0600

    updated

--- routes/auth.js
+++ routes/auth.js
@@ -11,6 +11,7 @@ const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 router.use(validarJWT);
+router.get('/',(req,res)=>{res.status(200).json({ok:'Bienvenido'})})
 router.post(
     '/new',
     [//middlewares
